9ee0cbe77c5d23d2260f3e07879b0332bbed008c
Alan <alan@codecrunchers.ie>
2017-10-12 21:30:35 +0100

    dev

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,5 @@
-var http = require('http');
+var http = require("http")
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(8000);
-console.log('Server running on port 8000.');
+	res.writeHead(200, {"Content-Type": "text/plain"})
+	res.end("Hello World\n")
+}).listen(8000)
